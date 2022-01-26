a3d9ec9727561c1ec59061090bd1a2025ada0a71
mikolajguszpit <58148832+mikolajguszpit@users.noreply.github.com>
2022-01-26 11:57:02 +0100

    sprawdzanie stanu usb oraz odbiornika

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -239,6 +239,9 @@ int main(void)
   MX_SPI1_Init();
   MX_TIM14_Init();
   /* USER CODE BEGIN 2 */
+	
+  arm_fir_init_q31(&firsetting1, FILTER_NUM, &filter_tab[0], &fir_state1[0], BLOCK_SIZE_FLOAT);
+  arm_fir_init_q31(&firsetting2, FILTER_NUM, &filter_tab[0], &fir_state2[0], BLOCK_SIZE_FLOAT);
 
   /* USER CODE END 2 */
 
@@ -246,6 +249,121 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	if (rxstate==1) {
+
+		PDM_Filter(&pdmRxBuf[0],ptr_MidBuffer, &PDM1_filter_handler);
+	 	PDM_Filter(&pdmRxBuf[64],ptr_MidBuffer+16, &PDM1_filter_handler);
+	 	PDM_Filter(&pdmRxBuf[128],ptr_MidBuffer+32, &PDM1_filter_handler);
+
+	 	for (int i=0; i<PDM_SIZE/8;++i) {
+
+	 	    	FifoWrite_Q31();
+	 	    	MidBuffer_1[i]= ((int) (MidBuffer[i]<<16)|0x0000);
+
+	 	}
+
+	 	PDM_Filter(&pdmRxBuf2[0],ptr_MidBuffer, &PDM2_filter_handler);
+	 	PDM_Filter(&pdmRxBuf2[64],ptr_MidBuffer+16, &PDM2_filter_handler);
+	 	PDM_Filter(&pdmRxBuf2[128],ptr_MidBuffer+32, &PDM2_filter_handler);
+
+	 	for (int i=0; i<PDM_SIZE/8;++i) {
+
+	 	    	FifoWrite_Q31_2();
+	 	    	MidBuffer_2[i]= ((int) (MidBuffer[i]<<16)|0x0000);
+
+	 	}
+
+
+
+	 	fir_w=fifo_w_ptr;
+	 	rxstate=0;
+
+
+
+	}
+
+	if (rxstate==2) {
+
+ 	      	PDM_Filter(&pdmRxBuf[192],ptr_MidBuffer, &PDM1_filter_handler);
+ 	      	PDM_Filter(&pdmRxBuf[256],ptr_MidBuffer+16, &PDM1_filter_handler);
+ 	      	PDM_Filter(&pdmRxBuf[320],ptr_MidBuffer+32, &PDM1_filter_handler);
+
+ 	      	for (int i=0; i<PDM_SIZE/8;++i) {
+
+ 	      		FifoWrite_Q31();
+ 	      		MidBuffer_1[i]= ((int) (MidBuffer[i]<<16)|0x0000);
+
+ 	      	}
+
+ 	      	PDM_Filter(&pdmRxBuf2[192],ptr_MidBuffer, &PDM2_filter_handler);
+ 	      	PDM_Filter(&pdmRxBuf2[256],ptr_MidBuffer+16, &PDM2_filter_handler);
+ 	      	PDM_Filter(&pdmRxBuf2[320],ptr_MidBuffer+32, &PDM2_filter_handler);
+
+ 	      	for (int i=0; i<PDM_SIZE/8;++i) {
+
+ 	      		FifoWrite_Q31_2();
+ 	      		MidBuffer_2[i]= ((int) (MidBuffer[i]<<16)|0x0000);
+
+ 	      	}
+
+ 	      	fir_w=fifo_w_ptr;
+ 	      	rxstate=0;
+	 }
+
+
+	 if (fir_w > 0){
+	  	  arm_fir_q31(&firsetting1, &MidBuffer_1[0] , &fifobuf_f[fir_w - BLOCK_SIZE_FLOAT], BLOCK_SIZE_FLOAT);
+	  	  arm_fir_q31(&firsetting2, &MidBuffer_2[0] , &fifobuf_f2[fir_w - BLOCK_SIZE_FLOAT], BLOCK_SIZE_FLOAT);
+
+
+	    	  if (fir_w-fifo_r_ptr>48 && DMA_runn==0 ) {DMA_runn = 1;}
+		    	  ++licznik;
+		    	  if(licznik%1000==0){
+		    		  diffrent = ((int16_t) fir_w) - ((int16_t)fifo_r_ptr);
+		    		  if(diffrent<0){diffrent=diffrent+BUFF_OGOLNY;}
+	   		  	  if(diffrent>96){
+		  		  	fifo_w_ptr = fifo_r_ptr + 96;
+	  		    	  	if (fifo_w_ptr>BUFF_OGOLNY){fifo_w_ptr=fifo_w_ptr-BUFF_OGOLNY;}
+	    			  	fifo_w_ptr2 = fifo_w_ptr;
+	  	 			}
+
+		  	  	  licznik = 0;
+	  	}
+	  	fir_w = 0;
+	  }
+	  if(usbstate == 1){
+		  
+	    	  USBD_AUDIO_HandleTypeDef *haudio = hUsbDeviceFS.pClassData;
+	    	  int16_t *buf_part = haudio->in_buffer + (AUDIO_IN_PACKET / 2) * haudio->in_buffer_half;  // USB mic buffer access
+
+	    	  for (uint16_t i = 0; i < (AUDIO_IN_PACKET / 2); i+=2) {
+
+	    		  //uint16_t temp = FifoRead_Q31();
+	  	  	  *(buf_part+i) = FifoRead_Q31();
+	  	  	  *(buf_part+i+1) = FifoRead_Q31_2();	
+		  }
+
+	    	  usbstate=0;
+
+	   }
+
+	   if(usbstate == 2){ //gdy USB potrzebuje danych a ich jeszcze niema
+
+	    	  USBD_AUDIO_HandleTypeDef *haudio = hUsbDeviceFS.pClassData;
+	    	  int16_t *buf_part = haudio->in_buffer + (AUDIO_IN_PACKET / 2) * haudio->in_buffer_half;  // USB mic buffer access
+	    	  //pobranie adresu do wypelnienia
+
+	    	  for (uint16_t i = 0; i < (AUDIO_IN_PACKET / 2); i+=2) {
+
+	  	  	*(buf_part+i) = 0;
+	  	  	*(buf_part+i+1) = 0;
+
+	  	  }
+
+	    	  usbstate=0;
+	    }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
